2d01ddc2173b8736b0fd72b99539fa664551eaec
Andreas Pätzold <andi.paetzold@gmail.com>
2015-01-03 00:06:50 +0100

    added a timer to the top left corner

--- script/gameplay.js
+++ script/gameplay.js
@@ -21,14 +21,24 @@ DodgeIt.prototype.gameplay_start = function(container)
             // check new position
             this.character.x = Math.max(this.character.x, 0);
             this.character.x = Math.min(this.character.x, this.width - this.character.width);
+
+            // inc time
+            this.time += delta;
         },
 
         render: function()
         {
+            this.layer.clear("#FFFFFF");
+
+            // character
             this.layer
-                .clear("#FFFFFF")
                 .fillStyle("#000")
                 .fillRect(this.character.x, this.height - this.character.height, this.character.width, this.character.height);
+
+            // text
+            this.layer
+                .font("16px Arial")
+                .wrappedText(Math.round(this.time).toString() + "s", 5, 16);
         },
 
         // character
@@ -36,6 +46,9 @@ DodgeIt.prototype.gameplay_start = function(container)
             width:  64,
             height: 64,
             x: 0
-        }
+        },
+
+        // time
+        time: 0
     });
 };
