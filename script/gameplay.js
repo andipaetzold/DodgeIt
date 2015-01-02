052df2bfce30ea52739f60a557547660fd8ac60e
Andreas Pätzold <andi.paetzold@gmail.com>
2015-01-02 23:55:53 +0100

    added simple keyboard-controlled object

--- script/gameplay.js
+++ script/gameplay.js
@@ -8,11 +8,34 @@ DodgeIt.prototype.gameplay_start = function(container)
 
         step: function(delta)
         {
+            // move character
+            if (this.keyboard.keys.left && !this.keyboard.keys.right)
+            {
+                this.character.x -= 300 * delta;
+            }
+            else if (!this.keyboard.keys.left && this.keyboard.keys.right)
+            {
+                this.character.x += 300 * delta;
+            }
+
+            // check new position
+            this.character.x = Math.max(this.character.x, 0);
+            this.character.x = Math.min(this.character.x, this.width - this.character.width);
         },
 
         render: function()
         {
-            this.layer.clear("#FFFFFF")
+            this.layer
+                .clear("#FFFFFF")
+                .fillStyle("#000")
+                .fillRect(this.character.x, this.height - this.character.height, this.character.width, this.character.height);
+        },
+
+        // character
+        character: {
+            width:  64,
+            height: 64,
+            x: 0
         }
     });
 };
