8da6c25b3e0084d064defa54c92a206c547d4744
Andreas Pätzold <andi.paetzold@gmail.com>
2015-01-18 21:34:28 +0100

    necessary scripts won't be loaded via javascripts anymore

--- script/dodgeit.js
+++ script/dodgeit.js
@@ -20,28 +20,6 @@ function DodgeIt(container)
     this.container = container;
     this.container.children().hide();
 
-    // scripts to load
-    var scripts = [
-        "script/include/jquery.cookie.js",
-        "script/screen.js",
-        "script/include/canvasquery.js",
-        "script/include/playground.js",
-        "script/gameplay.js",
-        "script/audio.js",
-        "script/controls.js",
-        "script/leaderboard.js"
-    ];
-
-    // load
-    $.each(scripts, function(index, value)
-    {
-        $.ajax({
-            url: value,
-            dataType: "script",
-            async: false
-        });
-    });
-
     // default options
     this.options = {
         style: "car"
